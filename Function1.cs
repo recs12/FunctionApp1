aaa7fa4ec91d4fc594d16ed82293d2f34574845a
Slimane Rechdi <recs@premiertech.com>
2023-11-08 10:41:19 -0500

    renaming

--- Function1.cs
+++ Function1.cs
@@ -12,7 +12,7 @@ namespace FunctionApp1
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("Greatings")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
@@ -20,14 +20,16 @@ namespace FunctionApp1
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
+            string surname = req.Query["surname"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+            surname = surname ?? data?.surname;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}!!!";
+            string responseMessage = string.IsNullOrEmpty(name) || string.IsNullOrEmpty(surname)
+                ? "This HTTP triggered function executed successfully. Pass a name and surname in the query string or in the request body for a personalized response."
+                : $"Hello, {name} {surname}!!!";
 
             return new OkObjectResult(responseMessage);
         }
